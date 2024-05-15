afbb176c072491aa080db6afa18b274f0d8ccb44
Kavindu <kavinduhirushan2477025@gmail.com>
2024-05-15 20:05:59 +0530

    TopMeels in assets

---
+++ sport_managament_system/frontend/src/Assets/TopMeels.jsx
@@ -0,0 +1,28 @@
+export const topMeals = [
+
+    {
+        image: "https://images.pexels.com/photos/209977/pexels-photo-209977.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "BADMINTON"
+    },
+    {
+        image: "https://images.pexels.com/photos/262524/pexels-photo-262524.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "FOOTBALL"
+    },
+    {
+        image: "https://images.pexels.com/photos/949126/pexels-photo-949126.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "GYM"
+    },
+    {
+        image: "https://images.pexels.com/photos/33703/relay-race-competition-stadium-sport.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "ATHLATIC"
+    },
+    {
+        image: "https://images.pexels.com/photos/863988/pexels-photo-863988.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "SWIMING"
+    },
+    {
+        image: "https://images.pexels.com/photos/3718433/pexels-photo-3718433.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "CRICKET"
+    },
+
+]
