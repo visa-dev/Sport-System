f55c6288e4e3c2c175723da668573bc5e54e0f73
Viraj Sachin <147372921+visa-dev@users.noreply.github.com>
2024-05-15 12:15:21 +0530

    Create Folders

---
+++ sport_managament_system/frontend/src/Assets/Data/FooterItems.jsx
@@ -0,0 +1,110 @@
+import { RiLinkedinFill } from 'react-icons/ri';
+import { AiFillYoutube, AiFillGithub, AiOutlineInstagram } from 'react-icons/ai';
+
+const socialLinks = [
+
+    {
+        path: "https://www.youtube.com",
+        icon: <AiFillYoutube className='group-hover:text-white w-4 h-4' />
+    },
+    {
+        path: "https://www.github.com",
+        icon: <AiFillGithub className='group-hover:text-white w-4 h-4' />
+    },
+    {
+        path: "https://www.instagram.com",
+        icon: <AiOutlineInstagram className='group-hover:text-white w-4 h-4' />
+    },
+    {
+        path: "https://www.linkedin.com",
+        icon: <RiLinkedinFill className='group-hover:text-white w-4 h-4' />
+    },
+
+];
+
+const contactData = [
+
+    {
+        path: "",
+        display: 'temp'
+    },
+    {
+        path: "",
+        display: 'temp'
+    },
+    {
+        path: "",
+        display: 'temp'
+    },
+    {
+        path: "",
+        display: 'temp'
+    },
+
+];
+
+
+const quickLink02 = [
+
+    {
+        path: "",
+        display: 'temp'
+    },
+    {
+        path: "",
+        display: 'temp'
+    },
+    {
+        path: "",
+        display: 'temp'
+    },
+    {
+        path: "",
+        display: 'temp'
+    },
+
+];
+
+const quickLink01= [
+
+    {
+        path: "",
+        display: 'temp'
+    },
+    {
+        path: "",
+        display: 'temp'
+    },
+    {
+        path: "",
+        display: 'temp'
+    },
+    {
+        path: "",
+        display: 'temp'
+    },
+
+];
+
+const quickLink03 = [
+
+    {
+        path: "",
+        display: 'temp'
+    },
+    {
+        path: "",
+        display: 'temp'
+    },
+    {
+        path: "",
+        display: 'temp'
+    },
+    {
+        path: "",
+        display: 'temp'
+    },
+
+];
+
+export { socialLinks, contactData,quickLink01,quickLink02,quickLink03};
---
+++ sport_managament_system/frontend/src/Assets/Data/HeaderItems.jsx
@@ -0,0 +1,68 @@
+const addminNavLinks = [
+    {
+        path: '/admin/achivement',
+        display: 'ACHIVEMENT'
+
+    },
+    {
+        path: '/admin/coach',
+        display: 'COACH '
+
+    },
+    {
+        path: '/admin/equipment',
+        display: 'EQUIPMENT'
+
+    },
+    {
+        path: '/admin/sport',
+        display: 'SPORT'
+
+    },
+
+    {
+        path: '/admin/shedule',
+        display: 'SHEDULE'
+
+    },
+    {
+        path: '/admin/score',
+        display: 'SCORE'
+
+    }, {
+        path: '/admin/reservation',
+        display: 'RESERVATION'
+    }
+
+]
+
+const userNavLinks = [
+    {
+        path: '/',
+        display: 'HOME'
+
+    },
+    {
+        path: '/user/uov-score',
+        display: 'SCORES'
+
+    },
+    {
+        path: '/user/uov-coach',
+        display: 'COACH'
+
+    },
+    // {
+    //     path: '/user/uov-shedule',
+    //     display: 'SHEDULE'
+
+    // },
+    {
+        path: '/user/uov-equepments',
+        display: 'EQUIPMENTS'
+
+    }
+]
+
+
+export { addminNavLinks, userNavLinks }
